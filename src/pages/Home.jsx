4cd131210ad1e3216b8f394114181487997b7d84
Jiehao Luo <powerseed5044@gmail.com>
2023-11-19 14:09:45 -0700

    add audio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
@@ -7,10 +7,27 @@ import Sky from '../models/Sky'
 import Bird from '../models/Bird'
 import Plane from '../models/Plane'
 import HomeInfo from '../components/HomeInfo'
+import sakura from '../assets/sakura.mp3'
+import { soundoff, soundon } from '../assets/icons'
 
 const Home = () => {
+    const audioRef = useRef(new Audio(sakura));
+    audioRef.current.volume = 0.4;
+    audioRef.current.loop = true;
+
     const [currentStage, setCurrentStage] = useState(1);
     const [isRotating, setIsRotating] = useState(false)
+    const [isPlayingMusic, setIsPlayingMusic] = useState(false);
+
+    useEffect(() => {
+        if (isPlayingMusic) {
+            audioRef.current.play();
+        }
+
+        return () => {
+            audioRef.current.pause();
+        }
+    }, [isPlayingMusic])
 
     const adjustIslandForScreenSize = () => {
         let screenScale = null;
@@ -81,6 +98,15 @@ const Home = () => {
                     />
                 </Suspense>
             </Canvas>
+
+            <div className='absolute bottom-2 left-2'>
+                <img 
+                    src={!isPlayingMusic ? soundoff : soundon}
+                    alt='sound'
+                    className='w-10 h-10 cursor-pointer object-contain'
+                    onClick={() => setIsPlayingMusic(!isPlayingMusic)}
+                />
+            </div>
         </section>
     )
 }
